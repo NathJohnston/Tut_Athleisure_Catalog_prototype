3f9d1408566449f44614772c2944befecf852d80
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-08-11 08:31:49 +0800

    Update streamlit_app.py
    Connect to Snowflake database based on Streamlit Secrets file
    declare a cursor
    execute SQL code to populate the cursor
    print text in Streamlit and data from cursor

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 import snowflake.connector
 streamlit.title('My Parents New Healthy Diner')
+
+my_cnn = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnn.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
